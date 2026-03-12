fed530114204f3b210eb998b85d574f7b41c0bb1
Christian Armenta <car_arca@hotmail.com>
2026-03-11 17:12:25 -0700

    feat: added react-icons to the app and remove unused function on App..tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/layout/Header/Header";
-import { useIsTablet } from "./hooks/useMediaQuery";
 import { AppRoutes } from "./app/router/routes";
 import { SnackbarProvider } from "notistack";
 
